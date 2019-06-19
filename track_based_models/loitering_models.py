02f56114e28d9586badc9dc10bb4beb5997f604a
bitsofbits <tim@bitsofbits.com>
2019-06-18 20:32:27 -0700

    bug fixes

--- track_based_models/loitering_models.py
+++ track_based_models/loitering_models.py
@@ -86,67 +86,71 @@ class LoiteringModelV1(SingleTrackModel):
 
 
 
-    @classmethod
-    def generate_data(cls, paths, min_samples, label_window=None, seed=888, 
-                    skip_label=False, keep_fracs=(1,), noise=None, 
-                    precomp_features=None, vessel_label=None):
-        window = cls.window
-        delta = cls.delta
-        if label_window is None:
-            label_window = delta
-        assert window % delta == 0, 'delta must evenly divide window'
-        assert label_window % delta == 0, 'delta must evenly divide label_window'
-        assert window >= label_window, "window must be at least as large as label_window"
-        # Weight so that sets with multiple classification get sqrt(n) more representation
-        # Since they have some extra information (n is the number of classifications)
-        subsamples = int(round(min_samples / np.sqrt(len(paths))))
-        # Set seed for reproducibility
-        np.random.seed(seed)
-        times = []
-        features = []
-        transshipping = []
-        labels = []
-        defined = []
-        window_pts = window // delta
-        lbl_pts = label_window // delta
-        lbl_offset = (window_pts - lbl_pts) // 2
-        min_ndx = 1
-        for p in paths:
-            for data in cls.load_data(p, delta, skip_label, 
-                                    keep_fracs=keep_fracs, 
-                                    features=precomp_features, 
-                                    vessel_label=vessel_label):
-                if data is None:
-                    print('skipping', p)
-                    continue
-                (t, x, y, label, dfnd) = data
-                
-                max_ndx = len(x) - window_pts
-                ndxs = []
-                for ndx in range(min_ndx, max_ndx + 1):
-                    if dfnd[ndx:ndx+window_pts].sum() >= window_pts // 2:
-                        ndxs.append(ndx)
-                if not ndxs:
-                    print("skipping", p, "because it is too short")
-                    continue
-                for ss in range(subsamples):
-                    ndx = np.random.choice(ndxs)                
-                    t_chunk = t[ndx:ndx+window_pts]
-                    f_chunk, _ = cls.cook_features(y[ndx:ndx+window_pts])
-                    times.append(t_chunk) 
-                    features.append(f_chunk)
-                    if skip_label:
-                        transshipping.append(None)
-                        labels.append(None)
-                        defined.append(None)
-                    else:
-                        transshipping.append(label[ndx:ndx+window_pts]) 
-                        windowed_labels = label[ndx+lbl_offset:ndx+lbl_offset+lbl_pts]
-                        labels.append(windowed_labels.mean() > 0.5)
-                        windowed_defined = dfnd[ndx+lbl_offset:ndx+lbl_offset+lbl_pts]
-                        defined.append(windowed_defined.mean() > 0.5)
-        return times, np.array(features), np.array(labels), np.array(transshipping), np.array(defined)  ### CHANGE
+class LoiteringModelV2(SingleTrackModel):
+    
+    delta = 10 * minute
+    window = 12 * hour + delta
+    time_points = window // delta
 
+    base_filter_count = 64
+
+    data_source_lbl='transshiping' 
+    data_target_lbl='is_target_encounter'
+    data_undefined_vals = (0, 3)
+    data_defined_vals = (1, 2)
+    data_true_vals = (1,)
+    data_false_vals = (2,)
+    data_far_time = 3 * 10 * minute
+    
+    def __init__(self):
+        
+        self.normalizer = None
+        
+        depth = self.base_filter_count
+        
+        input_layer = Input(shape=(self.time_points, 6))
+        y = input_layer
+        y = Conv1D(depth, 7)(y)
+        y = ELU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = Conv1D(depth, 5)(y)
+        y = ELU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = hybrid_pool_layer_2(y)
+        
+        depth = 3 * depth // 2
+        y = Conv1D(depth, 5)(y)
+        y = ELU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = Conv1D(depth, 5)(y)
+        y = ELU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = hybrid_pool_layer_2(y)
+        
+        depth = 3 * depth // 2
+        y = Conv1D(depth, 5)(y)
+        y = ELU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = Conv1D(depth, 5)(y)
+        y = ELU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = hybrid_pool_layer_2(y)
+        
+        depth = 3 * depth // 2
+        y = Conv1D(depth, 1)(y)
+        y = ELU()(y)
+        y = Conv1D(depth, 1)(y)
+        y = ELU()(y)
+
+        y = Conv1D(1, 1)(y)
+        y = Activation('sigmoid')(y)
+
+        # 1 -> (3 -> 11) -> (23 -> 31) -> (63, 77) -> 73
+        output_layer = y
+        model = KerasModel(inputs=input_layer, outputs=output_layer[:, :, 0])
+        opt = optimizers.Nadam(lr=0.0005, schedule_decay=0.01)
+        model.compile(optimizer=opt, loss='binary_crossentropy', metrics=["accuracy"])
+        self.model = model  
 
 
 
--- track_based_models/single_track_model.py
+++ track_based_models/single_track_model.py
@@ -1,5 +1,7 @@
 import datetime
 import numpy as np
+import os
+import pandas as pd
 from . import util
 from .util import minute, lin_interp, cos_deg, sin_deg 
 from .base_model import BaseModel
@@ -128,7 +130,7 @@ class SingleTrackModel(BaseModel):
                          vessel_label=None):
         obj_tv = util.load_json_data(path, vessel_label=vessel_label)  
         obj = util.convert_from_legacy_format(obj_tv)
-        obj['transshiping'] = obj_tv['transshiping']
+        obj[cls.data_source_lbl] = obj_tv[cls.data_source_lbl]
         # if features is None:
         if features is not None:
             # Filter features down to just the ssvid / time span we want
@@ -141,7 +143,7 @@ class SingleTrackModel(BaseModel):
             i0 = np.searchsorted(features.timestamp, t0, side='left')
             i1 = np.searchsorted(features.timestamp, t1, side='right')
             features = features.iloc[i0:i1]
-            # Add transshiping data to features
+            # Add obj data to features
             cls.add_obj_data(obj, features)
             # Rename so we can use features as obj:
             obj = pd.DataFrame({
@@ -150,11 +152,11 @@ class SingleTrackModel(BaseModel):
                 'course' : features.course_degrees,
                 'lat' : features.lat,
                 'lon' : features.lon,
-                'transshiping' : features.transshiping,
+                cls.data_source_lbl : features[cls.data_source_lbl],
                 })
         for kf in keep_fracs:
             try:
-                t, x, y, label, is_defined = LoiteringModelV1.build_features(obj, delta=delta, 
+                t, x, y, label, is_defined = cls.build_features(obj, 
                                                 skip_label=skip_label, keep_frac=kf)
             except:
                 print('skipping', path, kf)
@@ -187,5 +189,68 @@ class SingleTrackModel(BaseModel):
             else:
                 source.append(cls.data_undefined_vals[0])
         features[cls.data_source_lbl] = source
+
+
+    @classmethod
+    def generate_data(cls, paths, min_samples, label_window=None, seed=888, 
+                    skip_label=False, keep_fracs=(1,), noise=None, 
+                    precomp_features=None, vessel_label=None):
+        window = cls.window
+        delta = cls.delta
+        if label_window is None:
+            label_window = delta
+        assert window % delta == 0, 'delta must evenly divide window'
+        assert label_window % delta == 0, 'delta must evenly divide label_window'
+        assert window >= label_window, "window must be at least as large as label_window"
+        # Weight so that sets with multiple classification get sqrt(n) more representation
+        # Since they have some extra information (n is the number of classifications)
+        subsamples = int(round(min_samples / np.sqrt(len(paths))))
+        # Set seed for reproducibility
+        np.random.seed(seed)
+        times = []
+        features = []
+        targets = []
+        labels = []
+        defined = []
+        window_pts = window // delta
+        lbl_pts = label_window // delta
+        lbl_offset = (window_pts - lbl_pts) // 2
+        min_ndx = 1
+        for p in paths:
+            for data in cls.load_data(p, delta, skip_label, 
+                                    keep_fracs=keep_fracs, 
+                                    features=precomp_features, 
+                                    vessel_label=vessel_label):
+                if data is None:
+                    print('skipping', p)
+                    continue
+                (t, x, y, label, dfnd) = data
+                
+                max_ndx = len(x) - window_pts
+                ndxs = []
+                for ndx in range(min_ndx, max_ndx + 1):
+                    if dfnd[ndx:ndx+window_pts].sum() >= window_pts // 2:
+                        ndxs.append(ndx)
+                if not ndxs:
+                    print("skipping", p, "because it is too short")
+                    continue
+                for ss in range(subsamples):
+                    ndx = np.random.choice(ndxs)                
+                    t_chunk = t[ndx:ndx+window_pts]
+                    f_chunk, _ = cls.cook_features(y[ndx:ndx+window_pts])
+                    times.append(t_chunk) 
+                    features.append(f_chunk)
+                    if skip_label:
+                        targets.append(None)
+                        labels.append(None)
+                        defined.append(None)
+                    else:
+                        targets.append(label[ndx:ndx+window_pts]) 
+                        windowed_labels = label[ndx+lbl_offset:ndx+lbl_offset+lbl_pts]
+                        labels.append(windowed_labels.mean() > 0.5)
+                        windowed_defined = dfnd[ndx+lbl_offset:ndx+lbl_offset+lbl_pts]
+                        defined.append(windowed_defined.mean() > 0.5)
+        return times, np.array(features), np.array(labels), np.array(targets), np.array(defined)  ### CHANGE
+
 
 
