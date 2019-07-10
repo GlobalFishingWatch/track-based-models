cbc5f59300185d2de1fd48d7da69a41fa0c32427
bitsofbits <tim@bitsofbits.com>
2019-07-10 13:20:00 -0700

    mean model

--- track_based_models/loitering_models.py
+++ track_based_models/loitering_models.py
@@ -8,7 +8,7 @@ import keras
 from keras.models import Model as KerasModel
 from keras.layers import Dense, Dropout, Flatten, ELU, ReLU, Input, Conv1D
 from keras.layers import BatchNormalization, MaxPooling1D, Concatenate
-from keras.layers import Cropping1D
+from keras.layers import Cropping1D, AveragePooling1D
 from keras.layers.core import Activation, Reshape
 from keras import optimizers
 from .util import hour, minute
@@ -1450,6 +1450,91 @@ class LoiteringModelV8Max(SingleTrackModel):
                       validation_split=validation_split, 
                       validation_data=validation_data,
                       verbose=verbose, callbacks=callbacks)
+
+class LoiteringModelV8Avg(SingleTrackModel):
+    
+    delta = 10 * minute
+    time_points = 21 # 72 = 12 hours, 120 = 20 hours, should be odd
+    time_point_delta = 1
+    window = time_points * delta
+
+    base_filter_count = 16
+
+    data_source_lbl='transshiping' 
+    data_target_lbl='is_target_encounter'
+    data_undefined_vals = (0, 3)
+    data_defined_vals = (1, 2)
+    data_true_vals = (1,)
+    data_false_vals = (2,)
+    data_far_time = 3 * 10 * minute
+    
+    def __init__(self):
+        
+        self.normalizer = None
+        
+        d1 = depth = self.base_filter_count
+        
+        input_layer = Input(shape=(None, 6))
+        y = input_layer
+        y = Conv1D(depth, 3)(y)
+        y = ReLU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = Conv1D(depth, 3)(y)
+        y = ReLU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+
+        y0 = Cropping1D((2, 2))(y)
+        y1 = AveragePooling1D(5, strides=1)(y)
+        y = Concatenate()([y0, y1])
+        y = Conv1D(depth, 1)(y)
+
+        y = Conv1D(depth, 3, dilation_rate=1)(y)
+        y = ReLU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = Conv1D(depth, 3, dilation_rate=1)(y)
+        y = ReLU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+
+        y0 = Cropping1D((2, 2))(y)
+        y1 = AveragePooling1D(5, strides=1)(y)
+        y = Concatenate()([y0, y1])
+        y = Conv1D(depth, 1)(y)
+
+        y = Conv1D(depth, 3, dilation_rate=1)(y)
+        y = ReLU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+        y = Conv1D(depth, 3, dilation_rate=1)(y)
+        y = ReLU()(y)
+        y = BatchNormalization(scale=False, center=False)(y)
+
+        # (1 - 5) (5 9) (9 13) (13 17) (13 21)
+
+        y = Conv1D(1, 1)(y)
+        y = Activation('sigmoid')(y)
+
+        model = KerasModel(inputs=input_layer, outputs=y)
+        opt = optimizers.Nadam(lr=0.002, schedule_decay=0.05)
+        # opt = keras.optimizers.SGD(lr=0.00001, momentum=0.9, 
+        #                                 decay=0.5, nesterov=True)
+
+        model.compile(optimizer=opt, loss='binary_crossentropy', 
+            metrics=["accuracy"], sample_weight_mode="temporal")
+        self.model = model  
+
+    def fit(self, x, labels, epochs=1, batch_size=32, sample_weight=None, 
+            validation_split=0, validation_data=0, verbose=1, callbacks=[]):
+        self.normalizer = Normalizer().fit(x)
+        x1 = self.preprocess(x)
+        l1 = np.asarray(labels).reshape(len(labels), -1, 1)
+        if validation_data not in (None, 0):
+            a, b, c = validation_data
+            validation_data = self.preprocess(a), b, c
+        return self.model.fit(x1, l1, epochs=epochs, batch_size=batch_size, 
+                        sample_weight=sample_weight,
+                      validation_split=validation_split, 
+                      validation_data=validation_data,
+                      verbose=verbose, callbacks=callbacks)
+
 
 class LoiteringModelV9(SingleTrackModel):
     
