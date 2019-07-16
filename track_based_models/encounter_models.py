275d8903a321b8aa82daf41a7da6fcd0be50e69a
bitsofbits <tim@bitsofbits.com>
2019-07-16 09:15:05 -0700

    add encounters model

--- track_based_models/encounter_models.py
+++ track_based_models/encounter_models.py
@@ -13,6 +13,9 @@ from keras import optimizers
 from keras import regularizers
 from keras.models import load_model
 
+from .dual_track_model import DualTrackModel
+from .util import minute, lin_interp, cos_deg, sin_deg 
+
 assert K.image_data_format() == 'channels_last'
 
 
@@ -55,27 +58,37 @@ def hybrid_pool_layer_2(x):
                                       MaxPooling1D(3, strides=2)(x), x2]))    
     
 
-class BaseModel(object):
+# class BaseModel(object):
     
-    def flatten(self, x):
-        x = np.asarray(x)
-        return x.reshape(x.shape[0], -1)
+#     def flatten(self, x):
+#         x = np.asarray(x)
+#         return x.reshape(x.shape[0], -1)
     
-    def save(self, path):
-        self.model.save(os.path.join(path, 'model.h5'))
-        self.normalizer.save(os.path.join(path, 'norm.npz'))
+#     def save(self, path):
+#         self.model.save(os.path.join(path, 'model.h5'))
+#         self.normalizer.save(os.path.join(path, 'norm.npz'))
         
-    @classmethod
-    def load(cls, path):
-        mdl = cls()
-        mdl.model = load_model(os.path.join(path, 'model.h5'))
-        mdl.normalizer = Normalizer.load(os.path.join(path, 'norm.npz'))
-        return mdl
+#     @classmethod
+#     def load(cls, path):
+#         mdl = cls()
+#         mdl.model = load_model(os.path.join(path, 'model.h5'))
+#         mdl.normalizer = Normalizer.load(os.path.join(path, 'norm.npz'))
+#         return mdl
 
 
-class ConvNetModel4(BaseModel):
+class ConvNetModel4(DualTrackModel):
     
-    time_points = window // delta
+    delta = 10 * minute
+    time_points = 60 
+    window = time_points * delta
+    time_point_delta = 4
+
+    data_source_lbl='transshiping' 
+    data_target_lbl='is_target_encounter'
+    data_true_vals = [1]
+    data_false_vals = [2, 3]
+    data_far_time = 3 * 10 * minute
+    # time_points = window // delta
     base_filter_count = 32
     fc_nodes = 128
     
@@ -85,7 +98,7 @@ class ConvNetModel4(BaseModel):
         
         depth = self.base_filter_count
         
-        input_layer = Input(shape=(self.time_points, 9))
+        input_layer = Input(shape=(None, 9))
         y = input_layer
         y = Conv1D(depth, 5)(y)
         y = ELU()(y)
@@ -111,20 +124,19 @@ class ConvNetModel4(BaseModel):
         y = Conv1D(depth, 5)(y)
         y = ELU()(y)
         y = keras.layers.BatchNormalization(scale=False, center=False)(y)
-        y = hybrid_pool_layer_2(y)
         
-        y = Flatten()(y)
-        
-        y = Dense(self.fc_nodes)(y)
+        y = Conv1D(self.fc_nodes, 1)(y)
         y = ELU()(y)
 
-        y = Dense(1)(y)
+        y = Conv1D(1, 1)(y)
         y = Activation('sigmoid')(y)
+
         output_layer = y
         model = KerasModel(inputs=input_layer, outputs=output_layer)
         opt = optimizers.Nadam(lr=0.0005, schedule_decay=0.01)
         #opt = optimizers.Adam(lr=0.01, decay=0.5)
-        model.compile(optimizer=opt, loss='binary_crossentropy', metrics=["accuracy"])
+        model.compile(optimizer=opt, loss='binary_crossentropy', metrics=["accuracy"],
+            sample_weight_mode="temporal")
         self.model = model  
     
     def preprocess(self, x):
@@ -133,7 +145,7 @@ class ConvNetModel4(BaseModel):
     def fit(self, x, labels, epochs=1, batch_size=32, sample_weight=None, validation_split=0, validation_data=0):
         self.normalizer = Normalizer().fit(x)
         x1 = self.preprocess(x)
-        l1 = np.asarray(labels).reshape((len(labels), 1))
+        l1 = np.asarray(labels).reshape((len(labels), -1, 1))
         if validation_data not in (None, 0):
             a, b, c = validation_data
             validation_data = self.preprocess(a), b, c
@@ -147,8 +159,17 @@ class ConvNetModel4(BaseModel):
     
 class ConvNetModel5(ConvNetModel4):
     
-    time_points = window // delta - 1
-    
+    delta = 10 * minute
+    time_points = 71 
+    window = (time_points + 1) * delta
+    time_point_delta = 1
+
+    data_source_lbl='transshiping' 
+    data_target_lbl='is_target_encounter'
+    data_true_vals = [1]
+    data_false_vals = [2, 3]
+    data_far_time = 3 * 10 * minute
+
     def preprocess(self, x):
         x = np.asarray(x) # 3 / 4
         dxy = x[:, 1:, 3:5] - x[:, :-1, 3:5]
