460aef7c987d2f8d83d3d466f40b7497e44bc0d8
bitsofbits <tim@bitsofbits.com>
2019-09-01 14:59:08 -0700

    tweak v13

--- track_based_models/loitering_models.py
+++ track_based_models/loitering_models.py
@@ -1994,7 +1994,7 @@ class LoiteringModelV13(SingleTrackModel):
     time_point_delta = 4
     window = time_points * delta
 
-    base_filter_count = 32
+    base_filter_count = 8
 
     data_source_lbl='transshiping' 
     data_target_lbl='is_target_encounter'
